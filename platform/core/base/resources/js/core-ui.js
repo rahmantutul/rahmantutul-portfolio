83a4e58b919366204641c26a833c574e7d208200
rahmantutul <rahmantutul50@gmail.com>
2025-06-25 18:50:53 +0600

    Update core-ui.js and add zelio theme assets

--- platform/core/base/resources/js/core-ui.js
+++ platform/core/base/resources/js/core-ui.js
@@ -1,17 +1,21 @@
-import '@tabler/core/src/js/src/autosize'
-import '@tabler/core/src/js/src/dropdown'
-import '@tabler/core/src/js/src/tooltip'
-import '@tabler/core/src/js/src/popover'
-import '@tabler/core/src/js/src/switch-icon'
-import '@tabler/core/src/js/src/tab'
+// platform/core/base/resources/js/core-ui.js
+
+// Import Bootstrap’s JS
+import 'bootstrap'
+
+// Import the compiled Tabler bundle
+import '@tabler/core/dist/js/tabler'
+
+// Expose globals (if needed elsewhere)
 import * as bootstrap from 'bootstrap'
-import * as tabler from '@tabler/core/src/js/src/tabler'
+import * as tabler    from '@tabler/core/dist/js/tabler'
 
 globalThis.bootstrap = bootstrap
-globalThis.tabler = tabler
+globalThis.tabler    = tabler
 
+// Your custom progress setup
 import setupProgress from './base/progress'
 
 setupProgress({
-    showSpinner: true,
+  showSpinner: true,
 })
